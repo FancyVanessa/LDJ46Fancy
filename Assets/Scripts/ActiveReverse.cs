e542689e696f715ba3899d2f5e2483ab57570220
Vesa Varvemaa <vesa.varvemaa@gmail.com>
2020-04-19 16:25:19 +0300

    Changes

---
+++ Assets/Scripts/ActiveReverse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveReverse : MonoBehaviour
+{
+    public void ActiveOnOff()
+    {
+        if(gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (!gameObject.activeSelf)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
